d41b8fe3374a8b5ecd2811dadcec345ac65bbddc
Rajib Chandra Das <prorajib6@gmail.com>
2022-02-23 19:28:21 +0600

    inventory-updated

---
+++ src/components/Inventory/Inverntory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Inventory.css';
+
+const Inverntory = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Inverntory;
